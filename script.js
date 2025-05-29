325511d648b3aae99232bd58e321275ecf0eb51f
e-DG <126411190+e-DG@users.noreply.github.com>
2025-05-29 15:06:35 -0400

    Fixed computerChoice funtion

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// Create computer's choice.
-let computerChoice = Math.round(Math.random() * 2);
-console.log(computerChoice);
 
-function resetComptChoice() {
+// Create computer's choice (it works)
+
+let computerChoice;
+
+function ComptChooses() {
     computerChoice = Math.round(Math.random() * 2);
-    console.log(computerChoice);
+    if (computerChoice === 0) {
+        computerChoice = "scissors";
+         console.log(computerChoice);
+    }
+    else if (computerChoice === 1) {
+        computerChoice = "rock";
+         console.log(computerChoice);
+    }
+    else {
+        computerChoice = "paper";
+         console.log(computerChoice);
+    } 
 }
+
+ComptChooses();
 
