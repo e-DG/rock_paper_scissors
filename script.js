0c5a57e00bc1ef7afa42989fa7d5f61da9ff112c
e-DG <126411190+e-DG@users.noreply.github.com>
2025-05-29 14:45:14 -0400

    Created computer's random choice

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create computer's choice.
+let computerChoice = Math.round(Math.random() * 2);
+console.log(computerChoice);
+
+function resetComptChoice() {
+    computerChoice = Math.round(Math.random() * 2);
+    console.log(computerChoice);
+}
+
