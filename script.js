8988067979a5913fb8f9032d097872ba146775c7
e-DG <126411190+e-DG@users.noreply.github.com>
2025-05-30 10:55:16 -0400

    Added playGame function

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-
-// Create computer's choice (it works)
-
 let computerChoice;
-let randomizedHumanChoice;
+let computerScore = 0;
+let humanScore = 0;
+
 function comptChooses() {
     computerChoice = Math.round(Math.random() * 2);
     if (computerChoice === 0) {
         computerChoice = "scissors";
-         console.log(computerChoice);
+        //  console.log(computerChoice);
     }
     else if (computerChoice === 1) {
         computerChoice = "rock";
-         console.log(computerChoice);
+        //  console.log(computerChoice);
     }
     else {
         computerChoice = "paper";
-         console.log(computerChoice);
+        //  console.log(computerChoice);
     } 
 }
 
 
-
-    comptChooses();
 
  function playRound(computerChoice, humanChoice) {
 
@@ -29,14 +26,25 @@ function comptChooses() {
         ||humanChoice === "paper" && computerChoice === "rock" 
         ||humanChoice === "rock" && computerChoice === "scissors" ){
         console.log ("You win!")
+        humanScore++;
     }
     else if (computerChoice === "scissors" && humanChoice === "paper" 
         ||computerChoice === "paper" && humanChoice === "rock" 
         ||computerChoice === "rock" && humanChoice === "scissors" ){
-        console.log ("Computer wins.")}
+        console.log ("Computer wins.")
+        computerScore++;
+        
+    }
     else {
         console.log ("Draft")
     }
  }
 
- playRound (computerChoice, "scissors" );
+function playGame () {
+    for (let i = 1; i <= 5; i++) {
+        comptChooses(); 
+        let humanChoice = prompt("Choose your weapon");
+        playRound(computerChoice, humanChoice)
+        console.log("Round number " + i + ". The score is: Human " + humanScore + " - " + computerScore + " Computer");
+    }
+}
